b285f8c30649373f88050acb1b1d1abc71762ec3
dom alexander <dom.alexan@gmail.com>
2023-04-15 23:10:10 -0600

    Imports icons as contact bar buttons

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -7,12 +7,16 @@ const Contact = () => {
     <div className="contact">
       <h2>contact me</h2>
       <a className="contactButton" target="_blank" href="https://www.linkedin.com/in/domit-alexander/">
-      <FontAwesomeIcon icon="envelope" style={{color: "#b8c2b9", cursor: "pointer"}} />
+        <FontAwesomeIcon icon="fa-brands fa-linkedin" style={{color: "#b8c2b9", cursor: "pointer"}} />
       </a>
-      <a className="contactButton" target="_blank" href="https://github.com/dreadnought-admin">github</a>
-      <a className="contactButton" target="_blank" href="https://medium.com/@domit.alexander">medium</a>
+      <a className="contactButton" target="_blank" href="https://github.com/dreadnought-admin">
+        <FontAwesomeIcon icon="fa-brands fa-square-github" style={{color: "#b8c2b9", cursor: "pointer"}} />
+      </a>
+      <a className="contactButton" target="_blank" href="https://medium.com/@domit.alexander">
+        <FontAwesomeIcon icon="fa-brands fa-medium" style={{color: "#b8c2b9", cursor: "pointer"}} />
+      </a>
       <button className="email" onClick={(e) => window.location.href="mailto:dom.alexan@gmail.com"}>
-      <FontAwesomeIcon icon="envelope" style={{color: "#b8c2b9", cursor: "pointer"}} />
+        <FontAwesomeIcon icon="envelope" style={{color: "#b8c2b9", cursor: "pointer"}} />
       </button>
       
     </div>
