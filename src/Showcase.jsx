936acacc6fdb04348214c4eaaaa1b4a4aac638fa
dom alexander <dom.alexan@gmail.com>
2023-06-07 16:04:09 -0600

    Changes anchor link href to null

--- src/Showcase.jsx
+++ src/Showcase.jsx
@@ -73,7 +73,7 @@ const Showcase = () => {
       </div> */}
 
       <div className="showcase-text">
-        <a className="header-link" href="#"><h1>This Website</h1></a>
+        <a className="header-link"><h1>This Website</h1></a>
         <section>
           <p>React + CSS</p>
           <p>a single-page react native portfolio. extremely original.</p>
